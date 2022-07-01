01f62a8972ab4744487ae720a77f74614010816e
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2022-07-01 21:07:39 +0700

    menambah tampilan pada dashboard

--- resources/views/partials/dashboard/master.blade.php
+++ resources/views/partials/dashboard/master.blade.php
@@ -2,13 +2,9 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">{{ $page }}</h1>
+    {{-- <h1 class="h3 mb-4 text-gray-800">{{ $page }}</h1> --}}
     {{-- Main Content --}}
-    <div class="card shadow mb-4">
-        <div class="card-body">
-            @yield('content')
-        </div>
-    </div>
+    @yield('content')
 </div>
 <!-- /.container-fluid -->
 
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('block_user');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login')->middleware('block_user');
 Route::post('/login', [AuthController::class, 'login'])->name('login.action')->middleware('block_user');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
