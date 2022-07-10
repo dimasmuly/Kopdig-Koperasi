a963d51cd17823d00dc21cf0e358198aff1a5628
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2022-07-10 09:29:36 +0700

    menghapus sweet alert

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\Localization::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \RealRashid\SweetAlert\ToSweetAlert::class,
+            // \RealRashid\SweetAlert\ToSweetAlert::class,
         ],
 
         'api' => [
