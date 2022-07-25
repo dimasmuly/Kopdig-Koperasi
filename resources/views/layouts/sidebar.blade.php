61a5469a7b58c7dc964e5eb8be9954703432bc5c
MOH IBNU ABDURROHMAN SUTIO <ibnuabdurrohmansutio@gmail.com>
2022-07-25 23:11:31 +0700

    Menyeleksi tampilan menu sesuai role
    Co-Authored-By: Tharixs Akbar <70683226+Tharixs@users.noreply.github.com>
    Co-Authored-By: E41200368_Nila Amalia_Gol A <74764135+nilamalia02@users.noreply.github.com>

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,9 +14,9 @@
 
         {{-- <i class="ri-record-circle-line"></i> --}}
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
-        id="vertical-hover">
-    </button>
-</div>
+            id="vertical-hover">
+        </button>
+    </div>
 
     <div id="scrollbar">
         <div class="container-fluid">
@@ -33,52 +33,68 @@
                 </li>
 
                 <li class="menu-title"><i class="ri-more-fill"></i> <span>Chairman</span></li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ request()->routeIs('dashboard.market*') ? 'text-success' : '' }}"
-                        href="{{ route('dashboard.market') }}">
-                        <i class="ri-store-line"></i> <span data-key="t-market">Market</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ request()->routeIs('dashboard.order') ? 'text-success' : '' }}"
-                        href="{{ route('dashboard.order') }}">
-                        <i class="ri-money-dollar-circle-line"></i> <span data-key="t-order">Order</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ request()->routeIs('dashboard.administrator') ? 'text-success' : '' }}"
-                        href="{{ route('dashboard.administrator') }}">
-                        <i class="ri-user-line"></i> <span data-key="t-Administrator">Administrator</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link {{ request()->routeIs('dashboard.mails') ? 'text-success' : '' }}"
-                        href="{{ route('dashboard.mails') }}">
-                        <i class="ri-mail-line"></i> <span data-key="t-Administrator">Mails</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="#sidebarAccount" class="nav-link {{ request()->routeIs('dashboard.management*') ? 'text-success collapse' : 'collapsed' }}" data-bs-toggle="collapse" role="button"
-                        aria-expanded="{{request()->routeIs('dashboard.management*') ? 'true' : 'false'}}" aria-controls="sidebarAccount" data-key="t-level-1.2"><i
-                            class="ri-bar-chart-box-line"></i> <span data-key="t-Management">Management</span>
-                    </a>
-                    <div class="collapse menu-dropdown {{request()->routeIs('dashboard.management*') ? 'show' : ''}}" id="sidebarAccount">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('dashboard.management.stash') }}" class="nav-link" data-key="t-level-2.1">Stashes</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('dashboard.management.loans') }}" class="nav-link" data-key="t-level-2.1">Loans</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('dashboard.management.installment') }}" class="nav-link" data-key="t-level-2.1">Installement</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('dashboard.management.dues') }}" class="nav-link" data-key="t-level-2.1">Dues</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+                @if (Auth::user()->role_id == 2 || Auth::user()->role_id == 6 || Auth::user()->role_id == 7)
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ request()->routeIs('dashboard.market*') ? 'text-success' : '' }}"
+                            href="{{ route('dashboard.market') }}">
+                            <i class="ri-store-line"></i> <span data-key="t-market">Market</span>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->role_id == 2 || Auth::user()->role_id == 7)
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ request()->routeIs('dashboard.order') ? 'text-success' : '' }}"
+                            href="{{ route('dashboard.order') }}">
+                            <i class="ri-money-dollar-circle-line"></i> <span data-key="t-order">Order</span>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->role_id == 2 || Auth::user()->role_id == 6 || Auth::user()->role_id == 7 || Auth::user()->role_id == 5)
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ request()->routeIs('dashboard.administrator') ? 'text-success' : '' }}"
+                            href="{{ route('dashboard.administrator') }}">
+                            <i class="ri-user-line"></i> <span data-key="t-Administrator">Administrator</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu-link {{ request()->routeIs('dashboard.mails') ? 'text-success' : '' }}"
+                            href="{{ route('dashboard.mails') }}">
+                            <i class="ri-mail-line"></i> <span data-key="t-Administrator">Mails</span>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->role_id == 2 || Auth::user()->role_id == 6 || Auth::user()->role_id == 7)
+                    <li class="nav-item">
+                        <a href="#sidebarAccount"
+                            class="nav-link {{ request()->routeIs('dashboard.management*') ? 'text-success collapse' : 'collapsed' }}"
+                            data-bs-toggle="collapse" role="button"
+                            aria-expanded="{{ request()->routeIs('dashboard.management*') ? 'true' : 'false' }}"
+                            aria-controls="sidebarAccount" data-key="t-level-1.2"><i class="ri-bar-chart-box-line"></i>
+                            <span data-key="t-Management">Management</span>
+                        </a>
+                        <div class="collapse menu-dropdown {{ request()->routeIs('dashboard.management*') ? 'show' : '' }}"
+                            id="sidebarAccount">
+                            <ul class="nav nav-sm flex-column">
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard.management.stash') }}" class="nav-link"
+                                        data-key="t-level-2.1">Stashes</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard.management.loans') }}" class="nav-link"
+                                        data-key="t-level-2.1">Loans</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard.management.installment') }}" class="nav-link"
+                                        data-key="t-level-2.1">Installement</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('dashboard.management.dues') }}" class="nav-link"
+                                        data-key="t-level-2.1">Dues</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                @endif
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="/logout" onclick="return(confirm('Are you sure?'))">
                         <i class="ri-logout-box-line"></i> <span data-key="t-Logout">Logout</span>
--- routes/web.php
+++ routes/web.php
@@ -28,16 +28,22 @@ Route::get('/login', [AuthController::class, 'index'])->name('auth.login')->midd
 Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.login.authenticate');
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-Route::middleware(['middleware' => 'auth', 'check-role:2,3,5,6,7'])->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('dashboard/market', [MarketController::class, 'index'])->name('dashboard.market');
-    Route::get('/market/{id}/products', [MarketController::class, 'products'])->name('dashboard.market.products');
-    Route::get('dashboard/order', [OrderController::class, 'index'])->name('dashboard.order');
-    Route::get('dashboard/administrator', [AdministratorController::class, 'index'])->name('dashboard.administrator');
-    Route::post('dashboard/administrator/search', [AdminController::class, 'search'])->name('dashboard.admin.search');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware(['middleware' => 'auth', 'check-role:2,5,6,7']);
+
+Route::get('dashboard/market', [MarketController::class, 'index'])->name('dashboard.market')->middleware(['middleware' => 'auth', 'check-role:2,6,7']);
+Route::get('/market/{id}/products', [MarketController::class, 'products'])->name('dashboard.market.products')->middleware(['middleware' => 'auth', 'check-role:2,6,7']);
+
+Route::get('dashboard/order', [OrderController::class, 'index'])->name('dashboard.order')->middleware(['middleware' => 'auth', 'check-role:2,6,7']);
+
+Route::get('dashboard/administrator', [AdministratorController::class, 'index'])->name('dashboard.administrator')->middleware(['middleware' => 'auth', 'check-role:2,6,5,7']);
+Route::post('dashboard/administrator/search', [AdminController::class, 'search'])->name('dashboard.admin.search')->middleware(['middleware' => 'auth', 'check-role:2,6,7,5']);
+
+// Managements
+Route::middleware(['midleware' => 'auth', 'check-role:2,6,7'])->group(function () {
     Route::get('dashboard/stash', [StashController::class, 'index'])->name('dashboard.management.stash');
     Route::get('dashboard/loans', [LoanController::class, 'index'])->name('dashboard.management.loans');
     Route::get('dashboard/installment', [InstallmentController::class, 'index'])->name('dashboard.management.installment');
     Route::get('dashboard/dues', [DuesController::class, 'index'])->name('dashboard.management.dues');
-    Route::get('dashboard/mails', [MailController::class, 'index'])->name('dashboard.mails');
 });
+
+Route::get('dashboard/mails', [MailController::class, 'index'])->name('dashboard.mails')->middleware(['middleware' => 'auth', 'check-role:2,5,6,7']);
